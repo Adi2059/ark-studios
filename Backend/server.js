90ea1d2064c53aa6a831874d18756a1ab3074d80
Adi2059 <thakuraditya2059@gmail.com>
2026-06-13 01:51:46 +0530

    Forced global IPv4 using dns.setDefaultResultOrder

--- Backend/server.js
+++ Backend/server.js
@@ -1,3 +1,8 @@
+// 🚨 THE NUCLEAR FIX: Force Node.js to use IPv4 globally
+const dns = require('dns');
+dns.setDefaultResultOrder('ipv4first');
+
+
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
